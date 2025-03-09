8de073255054ad86feea959fee285dacb1e19f64
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2025-03-09 16:05:21 +0500

    add requirments.txt for backend

--- lib/providers/sound_provider.dart
+++ lib/providers/sound_provider.dart
@@ -1,23 +1,23 @@
-import 'package:flutter/material.dart';
-import '../models/sound_card_model.dart';
-import '../services/api_service.dart';
-
-class SoundProvider extends ChangeNotifier {
-  final ApiService _apiService = ApiService();
-  List<SoundCard> _buttons = [];
-  bool _isLoading = true;
-
-  List<SoundCard> get buttons => _buttons;
-  bool get isLoading => _isLoading;
-
-  Future<void> fetchButtons() async {
-    try {
-      _isLoading = true;
-      notifyListeners();
-      _buttons = await _apiService.fetchButtons();
-    } finally {
-      _isLoading = false;
-      notifyListeners();
-    }
-  }
-}
+// import 'package:flutter/material.dart';
+// import '../models/sound_card_model.dart';
+// import '../services/api_service.dart';
+//
+// class SoundProvider extends ChangeNotifier {
+//   final ApiService _apiService = ApiService();
+//   List<SoundCard> _buttons = [];
+//   bool _isLoading = true;
+//
+//   List<SoundCard> get buttons => _buttons;
+//   bool get isLoading => _isLoading;
+//
+//   Future<void> fetchButtons() async {
+//     try {
+//       _isLoading = true;
+//       notifyListeners();
+//       _buttons = await _apiService.fetchButtons();
+//     } finally {
+//       _isLoading = false;
+//       notifyListeners();
+//     }
+//   }
+// }
